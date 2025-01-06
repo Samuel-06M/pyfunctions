85ddad7397f446d6a5cdb6d7cdcbb0649960ebc3
Samuel-06M <samuelmwai031@gmail.com>
2025-01-06 09:51:10 +0300

    added some more functions

--- function.py
+++ function.py
@@ -1,3 +1,4 @@
+# subtraction function
 def subtract_numbers(a, b):
     result = a - b
     return result
@@ -6,5 +7,29 @@ difference_result = subtract_numbers(8, 10)
 
 print(difference_result)
 
+# multiplication function
+def multiplication(a, b):
+
+    return a * b
+
+result = multiplication(4, 5)
+print(result)
+
+# division function
+def division(a, b):
+    if b ==0:
+        raise ValueError("cannot divide by zero")
+    return a/b
+
+try:
+    result = division(50, 5)
+    print(result)
+except ValueError as e:
+    print(e)
 
 
+# addition function
+def addition(a,b):
+    return a + b
+result = addition(9, 9)
+print(result)
